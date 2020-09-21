8ae05e5ce50fa7235c65458014aa8ad4fb8d7817
Sumito Izumita <sumito@izumita.com>
2020-09-21 16:09:43 +0900

    メインページの作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer fixed app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -18,14 +24,14 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -33,6 +39,9 @@
 export default {
   data() {
     return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
       items: [
         {
           icon: 'mdi-apps',
